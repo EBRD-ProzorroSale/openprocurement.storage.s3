603b75069156438fb0752b60c9f37d9112010dc0
Vitalii Martyniak <vitalldh@gmail.com>
2018-08-08 13:45:11 +0300

    Set needed types for kwargs

--- openprocurement/storage/s3/__init__.py
+++ openprocurement/storage/s3/__init__.py
@@ -4,7 +4,9 @@ from openprocurement.storage.s3.storage import S3Storage
 
 def includeme(config):
     settings = config.registry.settings
-    s3_settings = {}
+    s3_settings = {
+        'is_secure': True
+    }
     if 's3.access_key' in settings and 's3.secret_key' in settings and 's3.bucket' in settings:
         # S3 connection
         for k, v in settings.items():
@@ -15,10 +17,15 @@ def includeme(config):
                 s3_settings['aws_secret_access_key'] = v
             elif key == 'access_key':
                 s3_settings['aws_access_key_id'] = v
+            elif key == 'port':
+                s3_settings[key] = int(v)
             elif key == 'bucket':
                 pass
             else:
                 s3_settings[key] = v
+
+        if 's3.is_secure' in settings and not settings['s3.is_secure']:
+            s3_settings['is_secure'] = False
         connection = S3Connection(**s3_settings)
         bucket_name = settings['s3.bucket']
         config.registry.storage = S3Storage(connection, bucket_name)
